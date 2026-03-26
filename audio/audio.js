3157e537ed40e81b7a2fc161af8e18850f11a10a
Fabziano <fabianotmp@gmail.com>
2026-03-26 13:43:21 -0300

    feat: player secundario

---
+++ audio/audio.js
@@ -0,0 +1,4 @@
+const faixas = [
+    { t: "OLTP vs OLAP", u: "https://raw.githubusercontent.com/fabziano/p/main/revisao/audio/1.wav" },
+    { t: "Dicas", u: "https://raw.githubusercontent.com/fabziano/p/main/revisao/audio/17.wav" }
+];
